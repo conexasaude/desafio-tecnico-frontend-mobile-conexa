eea17181ce66554bd9c320c363c67ffa5708be48
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-10-02 01:19:05 -0300

    feat: add EmptyList and ErrorScreen component

---
+++ src/presentation/components/empty-list/empty-list.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { routes } from '~/presentation/navigation/routes';
+import { useNavigation } from '@react-navigation/native';
+
+// Components
+import { Heading } from '~/presentation/components/heading';
+
+// Styles
+import { EmptyListContainer, StyledButton, StyledImage, WhiteHeading } from './styles';
+
+// Types
+import { StackNavigation } from '~/presentation/navigation/types';
+
+export function EmptyList() {
+  const navigation = useNavigation<StackNavigation>();
+  const emptyListSource = '../../../../assets/images/empty-list-image.png';
+
+  function goToCreateAppointmentScreen() {
+    navigation.navigate(routes.CreateAppointmentScreen);
+  }
+  return (
+    <EmptyListContainer>
+      <StyledImage source={require(emptyListSource)} alt="Lista vazia" />
+      <Heading type="H1">Não há consultas</Heading>
+      <StyledButton onPress={goToCreateAppointmentScreen}>
+        <WhiteHeading type="H2">Crie suas consultas</WhiteHeading>
+      </StyledButton>
+    </EmptyListContainer>
+  );
+}
