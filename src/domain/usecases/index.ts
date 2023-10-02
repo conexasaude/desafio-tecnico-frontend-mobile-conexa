160d0ac5463ec6acd456bbd997035d45a20a0969
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2023-10-02 09:41:57 -0300

    feat: create RemoteGetAppointmentsById

--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './authentication';
 export * from './create-appointment';
+export * from './get-appointments-by-id';
 export * from './get-appointments';
--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './remote-authentication-factory';
 export * from './remote-create-appointment-factory';
+export * from './remote-get-appointments-factory-by-id';
 export * from './remote-get-appointments-factory';
