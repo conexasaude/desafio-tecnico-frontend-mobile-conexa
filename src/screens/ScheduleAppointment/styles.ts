861b6661452d0b42ef7e56bbd42b863cd4f0c0e8
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:49:37 -0300

    feat: screen schedule appointment done

--- src/screens/ScheduleAppointment/styles.ts
+++ src/screens/ScheduleAppointment/styles.ts
@@ -1,9 +1,31 @@
 import styled from '@emotion/native';
 
-export const Container = styled.View({
+export const SafeArea = styled.SafeAreaView({
   flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
 });
 
-export const Title = styled.Text``;
+export const Container = styled.View(({ theme }) => ({
+  flex: 1,
+  paddingVertical: theme.effects.spacing.vl,
+  paddingHorizontal: theme.effects.spacing.md,
+}));
+
+export const Description = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.semiBold,
+  fontSize: theme.fonts.size.md,
+  color: theme.colors.gray[600],
+  marginBottom: theme.effects.spacing.md,
+}));
+
+export const Form = styled.View({
+  width: '100%',
+});
+
+export const Row = styled.View(({ theme }) => ({
+  flexDirection: 'row',
+  gap: theme.effects.spacing.md,
+}));
+
+export const Spacer = styled.View({
+  flex: 1,
+});
