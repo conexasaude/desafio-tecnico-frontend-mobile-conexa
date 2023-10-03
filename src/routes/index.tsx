c1c2c249e1c1a71b04728825528bf694252a2b0e
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 20:10:34 -0300

    feat: implements sync screen remember login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthStack } from '@/routes/auth';
 import { LoggedStack } from '@/routes/logged';
+import { useAsyncStorage } from '@/hooks/useAsyncStorage';
 import { useReduxSelector } from '@/hooks/useReduxSelector';
+import { useReduxDispatch } from '@/hooks/useReduxDispatch';
+import { userSlice } from '@/store/slices/user.slice';
+import { SyncScreen } from '@/screens/Sync';
 
 export function Routes() {
+  const [isSync, setIsSync] = useState(true);
   const { user } = useReduxSelector(state => state.user);
+  const { read } = useAsyncStorage();
+
+  const dispatch = useReduxDispatch();
+
+  const getUserAsyncStorage = useCallback(async () => {
+    const data = await read('user');
+
+    if (data) {
+      dispatch(userSlice.actions.rememberLogin(data));
+    }
+
+    setIsSync(false);
+  }, [dispatch, read]);
+
+  useEffect(() => {
+    getUserAsyncStorage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isSync) {
+    return <SyncScreen />;
+  }
 
   return (
     <NavigationContainer>
