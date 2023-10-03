a3f40f0d23f0276df9f6121cacbb9223759715c5
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:48:16 -0300

    feat: appointments screen done

--- src/screens/Appointments/index.tsx
+++ src/screens/Appointments/index.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useReduxSelector } from '@/hooks/useReduxSelector';
+import { useReduxDispatch } from '@/hooks/useReduxDispatch';
+import { FlatList } from 'react-native';
+import { Card } from '@/components/Card';
+import { useTheme } from '@emotion/react';
+import { useNavigation } from '@react-navigation/native';
+import { NavPropsLogged } from '@/routes/logged';
+import { fetchAppointments } from '@/store/thunks/appointment.thunk';
+import { useToast } from 'react-native-toast-notifications';
 import * as Styled from './styles';
 
 export function AppointmentsScreen() {
+  const { show } = useToast();
+  const { effects } = useTheme();
+  const { navigate } = useNavigation<NavPropsLogged>();
+  const { appointments } = useReduxSelector(state => state.appointment);
+
+  const dispatch = useReduxDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAppointments({ onError: show }));
+  }, [show, dispatch]);
+
   return (
-    <Styled.Container>
-      <Styled.Title>Appointments Screen</Styled.Title>
-    </Styled.Container>
+    <Styled.SafeArea>
+      <Styled.Container>
+        <Styled.Column>
+          <Styled.Title>Painel de Consultas</Styled.Title>
+          <Styled.Line />
+        </Styled.Column>
+
+        <Styled.Row>
+          <Styled.DescriptionList>{`${appointments.length} consulta(s) agendadas`}</Styled.DescriptionList>
+          <Styled.ScheduleButton
+            title="Agendar Consulta"
+            onPress={() => navigate('ScheduleAppointment')}
+          />
+        </Styled.Row>
+
+        <FlatList
+          data={appointments}
+          keyExtractor={({ id }) => id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              testID="appointment-card"
+              title={item.paciente}
+              description={item.dataConsulta}
+              button={{
+                title: 'Ver Detalhes',
+                onPress: () => navigate('DetailsAppointment', { id: item.id }),
+              }}
+            />
+          )}
+          contentContainerStyle={{ paddingHorizontal: effects.spacing.md }}
+        />
+      </Styled.Container>
+    </Styled.SafeArea>
   );
 }
--- src/screens/Appointments/styles.ts
+++ src/screens/Appointments/styles.ts
@@ -1,9 +1,46 @@
+import { Button } from '@/components/Button';
 import styled from '@emotion/native';
+
+export const SafeArea = styled.SafeAreaView({
+  flex: 1,
+});
 
 export const Container = styled.View({
   flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
 });
 
-export const Title = styled.Text``;
+export const Column = styled.View(({ theme }) => ({
+  marginTop: theme.effects.spacing.hg,
+  paddingHorizontal: theme.effects.spacing.md,
+}));
+
+export const Title = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.bold,
+  fontSize: theme.fonts.size.tl,
+  color: theme.colors.primary,
+}));
+
+export const Line = styled.View(({ theme }) => ({
+  height: 1,
+  width: '100%',
+  backgroundColor: theme.colors.gray[200],
+  marginTop: theme.effects.spacing.md,
+  marginBottom: theme.effects.spacing.hg,
+}));
+
+export const Row = styled.View(({ theme }) => ({
+  flexDirection: 'row',
+  alignItems: 'flex-end',
+  justifyContent: 'space-between',
+  marginBottom: theme.effects.spacing.vl,
+  paddingHorizontal: theme.effects.spacing.md,
+}));
+
+export const ScheduleButton = styled(Button)({
+  width: '40%',
+});
+
+export const DescriptionList = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.semiBold,
+  color: theme.colors.gray[600],
+}));
