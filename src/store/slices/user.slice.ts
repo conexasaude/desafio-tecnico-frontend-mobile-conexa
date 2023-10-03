491e5646ad96d10fb393fe3a6ef4ed39361474a9
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:29:21 -0300

    feat: create user store

---
+++ src/store/slices/user.slice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { User } from '@/models/user';
+import { login } from '../thunks/user.thunk';
+
+interface State {
+  loading: boolean;
+  error?: string;
+  user?: User;
+}
+
+const initialState: State = {
+  loading: false,
+  error: undefined,
+  user: undefined,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loggout: () => ({
+      loading: false,
+      error: undefined,
+      user: undefined,
+    }),
+  },
+  extraReducers: builder => {
+    builder.addCase(login.pending, (state, _) => {
+      state.loading = true;
+    });
+
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    });
+
+    builder.addCase(login.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload as string;
+    });
+  },
+});
