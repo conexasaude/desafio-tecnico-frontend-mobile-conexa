3cba4839d3360bb30ba09bf67331ad4ffece0be0
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:47:34 -0300

    feat: login screen done

--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,9 +1,35 @@
 import styled from '@emotion/native';
+import Logo from '../../../assets/svgs/logo-conexa.svg';
 
-export const Container = styled.View({
+interface SpacerProps {
+  flex?: number;
+}
+
+export const SafeArea = styled.SafeAreaView({
   flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
 });
 
-export const Title = styled.Text``;
+export const Container = styled.View(({ theme }) => ({
+  flex: 1,
+  paddingVertical: theme.effects.spacing.vl,
+  paddingHorizontal: theme.effects.spacing.md,
+}));
+
+export const Title = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.bold,
+  fontSize: theme.fonts.size.tl,
+  color: theme.colors.primary,
+  marginBottom: theme.effects.spacing.lg,
+}));
+
+export const Form = styled.View({
+  width: '100%',
+});
+
+export const LogoConexa = styled(Logo)({
+  alignSelf: 'center',
+});
+
+export const Spacer = styled.View<SpacerProps>(({ flex }) => ({
+  flex: flex ?? 1,
+}));
