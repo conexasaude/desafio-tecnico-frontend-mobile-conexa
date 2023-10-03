c10650637c36004497d1cebf04527125eb20fdec
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:50:29 -0300

    feat: screen profile done

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as Styled from './styles';
+import { useTheme } from '@emotion/react';
+import { useReduxSelector } from '@/hooks/useReduxSelector';
+import { useReduxDispatch } from '@/hooks/useReduxDispatch';
+import { userSlice } from '@/store/slices/user.slice';
 
 export function ProfileScreen() {
+  const { colors } = useTheme();
+  const { user } = useReduxSelector(state => state.user);
+  const dispatch = useReduxDispatch();
+
   return (
-    <Styled.Container>
-      <Styled.Title>Profile Screen</Styled.Title>
-    </Styled.Container>
+    <Styled.SafeArea>
+      <Styled.Container>
+        <Icon name="account-circle" size={64} color={colors.gray[400]} />
+        <Styled.LabelName>{user?.nome}</Styled.LabelName>
+        <Styled.LabelEmail>{user?.email}</Styled.LabelEmail>
+
+        <Styled.Spacer />
+
+        <Styled.LoggoutButton
+          variant="outlined"
+          title="Sair"
+          onPress={() => dispatch(userSlice.actions.loggout())}
+        />
+      </Styled.Container>
+    </Styled.SafeArea>
   );
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,9 +1,39 @@
+import { Button } from '@/components/Button';
 import styled from '@emotion/native';
 
-export const Container = styled.View({
+export const SafeArea = styled.SafeAreaView({
   flex: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
 });
 
-export const Title = styled.Text``;
+export const Container = styled.View(({ theme }) => ({
+  flex: 1,
+  alignItems: 'center',
+  paddingVertical: theme.effects.spacing.vl,
+  paddingHorizontal: theme.effects.spacing.md,
+}));
+
+export const Row = styled.View({
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: 8,
+});
+
+export const LabelName = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.bold,
+  fontSize: theme.fonts.size.lg,
+  color: theme.colors.gray[800],
+  marginTop: theme.effects.spacing.md,
+  marginBottom: theme.effects.spacing.nn,
+}));
+
+export const LabelEmail = styled.Text(({ theme }) => ({
+  fontFamily: theme.fonts.family.semiBold,
+  fontSize: theme.fonts.size.md,
+  color: theme.colors.gray[600],
+}));
+
+export const Spacer = styled.View({
+  flex: 1,
+});
+
+export const LoggoutButton = styled(Button)({});
