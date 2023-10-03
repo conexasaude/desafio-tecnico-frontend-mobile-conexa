966bbc7009906e97df1037bee3b229e3522c9b0a
Felipe Lima <felipe_lima10@icloud.com>
2023-10-03 11:15:49 -0300

    feat: configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['<rootDir>/__tests__/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|react-navigation|@react-navigation/.*|react-native-svg)',
+  ],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.ts',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/ios',
+    '<rootDir>/android',
+    '<rootDir>/__tests__/jest.setup.js',
+    '<rootDir>/node_modules',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/lib',
+    '<rootDir>/src/styles',
+    '<rootDir>/src/@types',
+    '<rootDir>/src/routes',
+    '<rootDir>/src/models',
+    'styles.ts',
+    'useReduxDispatch.ts',
+    'useReduxSelector.ts',
+  ],
 };
