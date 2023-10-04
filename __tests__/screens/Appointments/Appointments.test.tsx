fd10e02cad3005c2f2b1949cbd884d5b70e448cf
Felipe Lima <felipe_lima10@icloud.com>
2023-10-04 15:42:18 -0300

    test: implements test when error and when loading

--- __tests__/screens/Appointments/Appointments.test.tsx
+++ __tests__/screens/Appointments/Appointments.test.tsx
@@ -6,6 +6,7 @@ import {
 
 import { AppointmentsScreen } from '@/screens/Appointments';
 import { fireEvent } from '@testing-library/react-native';
+import { AppointmentState } from '@/store/slices/appointment.slice';
 
 const mockDispatch = jest.fn();
 const mockNavigate = jest.fn();
@@ -39,43 +40,51 @@ jest.mock('@/hooks/useReduxDispatch', () => ({
   useReduxDispatch: () => mockDispatch,
 }));
 
-jest.mock('@/hooks/useReduxSelector', () => ({
-  useReduxSelector: () => ({
-    loading: false,
-    error: undefined,
-    appointments,
-  }),
-}));
-
 jest.mock('@react-navigation/native', () => ({
   useNavigation: () => ({ navigate: mockNavigate }),
   useIsFocused: () => false,
 }));
 
 describe('Appointments', () => {
+  const mockedAppointmentStore: AppointmentState = {
+    loading: false,
+    error: undefined,
+    appointments,
+  };
+
   test('should render correctly', () => {
-    renderWithThemeProvider(<AppointmentsScreen />);
+    renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedAppointmentStore,
+    });
   });
 
   test('should matches snapshot', () => {
-    matchSnapshotWithProvider(<AppointmentsScreen />);
+    matchSnapshotWithProvider(<AppointmentsScreen />, {
+      appointment: mockedAppointmentStore,
+    });
   });
 
   test('should render all cards', () => {
-    const { getAllByTestId } = renderWithThemeProvider(<AppointmentsScreen />);
+    const { getAllByTestId } = renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedAppointmentStore,
+    });
     const appointmentsCard = getAllByTestId('appointment-card');
     expect(appointmentsCard.length).toEqual(2);
   });
 
   test('should call navigate to schedule appointment', () => {
-    const { getByText } = renderWithThemeProvider(<AppointmentsScreen />);
+    const { getByText } = renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedAppointmentStore,
+    });
     const scheduleAppointmentButton = getByText('Agendar Consulta');
     fireEvent.press(scheduleAppointmentButton);
     expect(mockNavigate).toHaveBeenCalledWith('ScheduleAppointment');
   });
 
   test('should call navigate to details appointment', () => {
-    const { getAllByTestId } = renderWithThemeProvider(<AppointmentsScreen />);
+    const { getAllByTestId } = renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedAppointmentStore,
+    });
     const appointmentsCard = getAllByTestId('appointment-card');
 
     appointmentsCard.forEach((card, index) => {
@@ -84,5 +93,33 @@ describe('Appointments', () => {
         id: appointments[index].id,
       });
     });
+  });
+
+  test('should render Loader', () => {
+    const mockedLoadingAppointmentStore: AppointmentState = {
+      appointments: [],
+      error: undefined,
+      loading: true,
+    };
+
+    const { getByTestId } = renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedLoadingAppointmentStore,
+    });
+
+    expect(getByTestId('loader')).toBeDefined();
+  });
+
+  test('should render ErrorBoundary', () => {
+    const mockedErrorAppointmentStore: AppointmentState = {
+      appointments: [],
+      error: 'Something went wrong',
+      loading: false,
+    };
+
+    const { getByTestId } = renderWithThemeProvider(<AppointmentsScreen />, {
+      appointment: mockedErrorAppointmentStore,
+    });
+
+    expect(getByTestId('error-boundary')).toBeDefined();
   });
 });
--- __tests__/screens/DetailsAppointment/DetailsAppointment.test.tsx
+++ __tests__/screens/DetailsAppointment/DetailsAppointment.test.tsx
@@ -5,6 +5,7 @@ import {
 } from '@/lib/functions/Testing';
 
 import { DetailsAppointmentScreen } from '@/screens/DetailsAppointment';
+import { AppointmentState } from '@/store/slices/appointment.slice';
 
 const mockDispatch = jest.fn();
 
@@ -24,23 +25,66 @@ jest.mock('@/hooks/useReduxDispatch', () => ({
   useReduxDispatch: () => mockDispatch,
 }));
 
-jest.mock('@/hooks/useReduxSelector', () => ({
-  useReduxSelector: () => ({ appointment }),
-}));
-
 describe('DetailsAppointments', () => {
+  const mockedAppointmentStore: AppointmentState = {
+    appointments: [],
+    appointment,
+    error: undefined,
+    loading: false,
+  };
+
   test('should render correctly', () => {
-    renderWithThemeProvider(<DetailsAppointmentScreen />);
+    renderWithThemeProvider(<DetailsAppointmentScreen />, {
+      appointment: mockedAppointmentStore,
+    });
   });
 
   test('should matches snapshot', () => {
-    matchSnapshotWithProvider(<DetailsAppointmentScreen />);
+    matchSnapshotWithProvider(<DetailsAppointmentScreen />, {
+      appointment: mockedAppointmentStore,
+    });
   });
 
   test('should render all informations about appointments', () => {
-    const { getByText } = renderWithThemeProvider(<DetailsAppointmentScreen />);
+    const { getByText } = renderWithThemeProvider(
+      <DetailsAppointmentScreen />,
+      { appointment: mockedAppointmentStore },
+    );
+
     expect(getByText('Maria Brow')).toBeDefined();
     expect(getByText('2020-04-23 08:30')).toBeDefined();
     expect(getByText('Exemplo de consulta')).toBeDefined();
+  });
+
+  test('should render Loader', () => {
+    const mockedLoadingAppointmentStore: AppointmentState = {
+      appointments: [],
+      appointment: undefined,
+      error: undefined,
+      loading: true,
+    };
+
+    const { getByTestId } = renderWithThemeProvider(
+      <DetailsAppointmentScreen />,
+      { appointment: mockedLoadingAppointmentStore },
+    );
+
+    expect(getByTestId('loader')).toBeDefined();
+  });
+
+  test('should render ErrorBoundary', () => {
+    const mockedErrorAppointmentStore: AppointmentState = {
+      appointments: [],
+      appointment: undefined,
+      error: 'Something went wrong',
+      loading: false,
+    };
+
+    const { getByTestId } = renderWithThemeProvider(
+      <DetailsAppointmentScreen />,
+      { appointment: mockedErrorAppointmentStore },
+    );
+
+    expect(getByTestId('error-boundary')).toBeDefined();
   });
 });
